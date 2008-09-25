5e29e725465d51cade9bbae3bcc636fc81d51468
Wolfgang Schnerring <ws@gocept.com>
2008-09-25 06:27:02 +0000

    initial skeleton

---
+++ setup.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2008 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+
+setup(
+    name='gocept.testdb',
+    version='0.1dev',
+    author='gocept',
+    author_email='mail@gocept.com',
+    description='Creates and drops temporary databases for testing purposes.',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    license='ZPL 2.1',
+    namespace_packages=['gocept'],
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        'SQLAlchemy',
+        ],
+    extras_require=dict(
+        test=[
+            'MySQL-python',
+            'psycopg2',
+            ]),
+)
