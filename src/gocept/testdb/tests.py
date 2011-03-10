d9cb282e5e7d1a4e8c85c8ab1d350987fe140ab4
Thomas Lotze <tl@gocept.com>
2011-03-10 06:35:42 +0000

    made existing test code for using a template db more expressive, added a test for force_template

--- src/gocept/testdb/tests.py
+++ src/gocept/testdb/tests.py
@@ -1,8 +1,9 @@
-# Copyright (c) 2008-2010 gocept gmbh & co. kg
+# Copyright (c) 2008-2011 gocept gmbh & co. kg
 # See also LICENSE.txt
 
 import doctest
 import shutil
+import sqlalchemy
 import tempfile
 import unittest
 
@@ -13,9 +14,32 @@ def write(path, content):
     f.close()
 
 
+def execute(dsn, cmd):
+    engine = sqlalchemy.create_engine(dsn)
+    conn = engine.connect()
+    result = conn.execute(cmd)
+    conn.invalidate()
+    conn.close()
+    return result
+
+
+def table_names(dsn):
+    engine = sqlalchemy.create_engine(dsn)
+    conn = engine.connect()
+    result = engine.table_names(connection=conn)
+    conn.invalidate()
+    conn.close()
+    return result
+
+
 def setUp(test):
     test.sql_dir = tempfile.mkdtemp()
-    test.globs.update(sql_dir=test.sql_dir, write=write)
+    test.globs.update(
+        sql_dir=test.sql_dir,
+        write=write,
+        execute=execute,
+        table_names=table_names,
+        )
 
 
 def tearDown(test):
