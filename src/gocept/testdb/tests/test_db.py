57d2c812261ff8c6e74e69146c9b89d114ccf5d7
Thomas Lotze <tl@gocept.com>
2013-05-22 07:28:26 +0200

    separated doctests from non-doctests to get rid of test-suite registration for the latter

--- src/gocept/testdb/tests/test_db.py
+++ src/gocept/testdb/tests/test_db.py
@@ -1,76 +1,9 @@
 # Copyright (c) 2008-2013 gocept gmbh & co. kg
 # See also LICENSE.txt
 
-import doctest
 import gocept.testdb.db
-import os
-import os.path
-import shutil
 import sqlalchemy
-import stat
-import subprocess
-import sys
-import tempfile
 import unittest
-import mock
-
-
-def write(path, content):
-    f = open(path, 'w')
-    f.write(content)
-    f.close()
-
-
-def execute(dsn, cmd):
-    engine = sqlalchemy.create_engine(dsn)
-    conn = engine.connect()
-    result = conn.execute(cmd)
-    conn.invalidate()
-    conn.close()
-    return result
-
-
-def table_names(dsn):
-    engine = sqlalchemy.create_engine(dsn)
-    conn = engine.connect()
-    result = engine.table_names(connection=conn)
-    conn.invalidate()
-    conn.close()
-    return result
-
-
-def system(test):
-    def system(cmdline):
-        args = cmdline.split()
-        args[0] = os.path.join(test.bin_dir, args[0])
-        subprocess.call(args)
-    return system
-
-
-def setUp(test):
-    test.bin_dir = tempfile.mkdtemp()
-    drop_all_path = os.path.join(test.bin_dir, 'drop-all')
-    write(drop_all_path, """\
-#!%s
-import sys
-sys.path = %r
-import gocept.testdb.cmdline
-gocept.testdb.cmdline.drop_all_entry_point()
-""" % (sys.executable, sys.path))
-    os.chmod(drop_all_path, os.stat(drop_all_path).st_mode | stat.S_IXUSR)
-    test.sql_dir = tempfile.mkdtemp()
-    test.globs.update(
-        sql_dir=test.sql_dir,
-        write=write,
-        execute=execute,
-        table_names=table_names,
-        system=system(test),
-        )
-
-
-def tearDown(test):
-    shutil.rmtree(test.sql_dir)
-    shutil.rmtree(test.bin_dir)
 
 
 class PostgreSQL(unittest.TestCase):
@@ -131,19 +64,3 @@ class StatusTests(unittest.TestCase):
         engine = sqlalchemy.create_engine(self.db.dsn)
         engine.connect().execute('DROP TABLE tmp_functest')
         self.assertFalse(self.db.is_testing)
-
-
-def test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(PostgreSQLRegressionTests))
-    suite.addTest(unittest.makeSuite(PostgreSQLTests))
-    suite.addTest(unittest.makeSuite(StatusTests))
-    suite.addTest(doctest.DocFileSuite(
-        'README.txt',
-        optionflags=doctest.ELLIPSIS
-                    | doctest.NORMALIZE_WHITESPACE
-                    | doctest.REPORT_NDIFF,
-        setUp=setUp,
-        tearDown=tearDown,
-        ))
-    return suite
