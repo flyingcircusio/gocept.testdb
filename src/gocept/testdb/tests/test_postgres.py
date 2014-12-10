ea560ff594236cf421a701cec0a9667afa712c0f
Michael Howitz <mh@gocept.com>
2014-12-10 15:19:12 +0100

    Improve tests

--- src/gocept/testdb/tests/test_postgres.py
+++ src/gocept/testdb/tests/test_postgres.py
@@ -1,40 +1,45 @@
-# Copyright (c) 2008-2013 gocept gmbh & co. kg
+# Copyright (c) 2008-2014 gocept gmbh & co. kg
 # See also LICENSE.txt
-
+from gocept.testdb.testing import unittest
 import gocept.testdb
+import gocept.testing.assertion
 import os
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
 
 
-class PostgreSQL(unittest.TestCase):
+class PostgreSQL(unittest.TestCase,
+                 gocept.testing.assertion.Exceptions):
 
     db_template = 'gocept.testdb.tests-template-%s' % os.getpid()
 
     def drop_all(self):
-        db = gocept.testdb.PostgreSQL(db_template=self.db_template)
-        db.drop_all(drop_template=True)
+        self.makeOne().drop_all(drop_template=True)
 
     setUp = tearDown = drop_all
+
+    def makeOne(self, db_template=None, lc_collate=None, prefix=None):
+        import gocept.testdb
+        if db_template is None:
+            db_template = self.db_template
+        return gocept.testdb.PostgreSQL(
+            db_template=db_template, lc_collate=lc_collate, prefix=prefix)
 
 
 class PostgreSQLRegressionTests(PostgreSQL):
 
     def test_template_db_doesnt_need_schema(self):
-        db = gocept.testdb.PostgreSQL(db_template=self.db_template)
-        db.create()
+        db = self.makeOne()
+        with self.assertNothingRaised():
+            db.create()
 
     def test_naming_scheme_matches_with_dash_in_prefix(self):
-        db = gocept.testdb.PostgreSQL(prefix='foo-bar')
+        db = self.makeOne(prefix='foo-bar')
         self.assertTrue(db._matches_db_naming_scheme('foo-bar-123'))
 
 
 class PostgreSQLTests(PostgreSQL):
 
     def test_can_add_database_with_special_lc_collate(self):
-        db = gocept.testdb.PostgreSQL(lc_collate='de_DE.UTF-8')
+        db = self.makeOne(lc_collate='de_DE.UTF-8')
         db.create()
         collate_by_db_name = dict((x[0], x[3]) for x in db.pg_list_db_items())
         self.assertEqual('de_DE.UTF-8', collate_by_db_name[db.db_name])
