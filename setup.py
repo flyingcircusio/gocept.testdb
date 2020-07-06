73f04575b144c34be3fa51e1ce6958e60db26abc
Michael Howitz <mh@gocept.com>
2020-07-06 15:53:22 +0200

    Preparing release 5.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require = [
 
 setup(
     name='gocept.testdb',
-    version='5.1.dev0',
+    version='5.1',
     author='gocept <mail at gocept dot com>',
     author_email='mail@gocept.com',
     description='Creates and drops temporary databases for testing purposes.',
